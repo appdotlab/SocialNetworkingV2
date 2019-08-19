4b7a80dc4c2356e49257cf423da8725e9c61b679
Abdul <jaseemabdul20@gmail.com>
2019-08-19 11:41:54 +0530

    Migration Complete

--- app/src/main/java/com/example/photouploader/MainActivity.java
+++ app/src/main/java/com/example/photouploader/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.photouploader;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +21,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -24,95 +30,67 @@ import com.google.firebase.storage.UploadTask;
 import java.io.IOException;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
-    Button chooseBtn, uploadBtn;
-    ImageView imageView;
-    private Uri filePath;
-    FirebaseStorage storage;
-    StorageReference storageReference;
+public class MainActivity extends AppCompatActivity { SharedPreferences prefs;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            Boolean islogout = false;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_newPost:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    selectedFragment = new AddPostFragment();
+                    break;
+                case R.id.navigation_Profile:
+                    selectedFragment = new HomeFragment();
+                    islogout = true;
+                    break;
+                case R.id.navigation_Search:
+                    selectedFragment = new SearchFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragments_container, selectedFragment).commit();
+            if(islogout == true) {
+                logout();
+            }
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bottom_nav);
 
-        chooseBtn = (Button) findViewById(R.id.chooseBtn);
-        uploadBtn = (Button) findViewById(R.id.uploadBtn);
-        imageView = (ImageView) findViewById(R.id.imageView);
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
-
-        upload();
-        choose();
+        String CU = getIntent().getStringExtra("CU");
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(navListener);
+        prefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragments_container, new HomeFragment()).commit();
 
     }
 
-    public void choose(){
-        chooseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 71);
-            }
-        });
+    public void logout(){
+        prefs.edit()
+                .remove("userID")
+                .remove("name")
+                .apply();
+        startActivity(new Intent(getApplicationContext(), loginActivity.class));
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 71 && resultCode == RESULT_OK
-                && data != null && data.getData() != null )
-        {
-            filePath = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                imageView.setImageBitmap(bitmap);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.i("Back", "Back Pressed");
+        getFragmentManager().popBackStackImmediate();
     }
-
-        public void upload(){
-            uploadBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(filePath != null)
-                    {
-                        final ProgressDialog progressDialog = new ProgressDialog(view.getContext());
-                        progressDialog.setTitle("Uploading...");
-                        progressDialog.show();
-
-                        StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
-                        ref.putFile(filePath)
-                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                    @Override
-                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                        progressDialog.dismiss();
-                                        Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        progressDialog.dismiss();
-                                        Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                                    @Override
-                                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                                        double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
-                                                .getTotalByteCount());
-                                        progressDialog.setMessage("Uploaded "+(int)progress+"%");
-                                    }
-                                });
-                    }
-                }
-            });
-        }
 }

0fdce23ede9468b90dcf769a749246a350943ca9
Ashwin <iamashwincherian@gmail.com>
2019-08-20 13:49:05 +0530

    User Profile

--- app/src/main/java/com/example/photouploader/MainActivity.java
+++ app/src/main/java/com/example/photouploader/MainActivity.java
@@ -4,31 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.IOException;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity { SharedPreferences prefs;
 
--- app/src/main/java/com/example/photouploader/UserProfile.java
+++ app/src/main/java/com/example/photouploader/UserProfile.java
@@ -1,7 +1,7 @@
 package com.example.photouploader;
 
 import android.content.Context;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,21 +11,61 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class UserProfile extends Fragment {
 
+    TextView usernameText, followersText, followingText;
+    Button editProfileBtn;
+    SharedPreferences prefs;
+    DatabaseReference userRef;
 
-    public UserProfile() {
-        // Required empty public constructor
-    }@Nullable
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        usernameText = (TextView) view.findViewById(R.id.usernameText);
+        followersText = (TextView) view.findViewById(R.id.followersText);
+        followingText = (TextView) view.findViewById(R.id.followingText);
+        prefs = view.getContext().getSharedPreferences("Prefs", Context.MODE_PRIVATE);
+        updateUI();
 
         return view;
+    }
+
+    private void updateUI(){
+        String userID = prefs.getString("userID", "N/A");
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = String.valueOf(dataSnapshot.child("name").getValue());
+                int followersCount = 0, followingCount = 0;
+                for(DataSnapshot userSnapshot : dataSnapshot.child("followers").getChildren()){
+                    followersCount++;
+                }
+                for(DataSnapshot userSnapshot : dataSnapshot.child("following").getChildren()){
+                    followingCount++;
+                }
+                usernameText.setText(String.valueOf(name));
+                followersText.setText(String.valueOf(followersCount));
+                followingText.setText(String.valueOf(followingCount));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
